a6eee66c6aad73821d91a43526ca046919056024
Jose Rayos(Tony) <tony.9464.tm@gmail.com>
2026-09-15 20:19:33 -0500

    Add contract and customer lookup routes

--- server/app.py
+++ server/app.py
@@ -6,5 +6,16 @@ contracts = [{"id": 1, "contract_information": "This contract is for John and bu
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+@app.route('/contract/<id>')
+def get_contract(id):
+    for contract in contracts:
+        if contract['id'] == int(id):
+            return contract["contract_information"], 200
+    return "", 404
+@app.route('/customer/<customer_name>')
+def get_customer(customer_name):
+    if customer_name in customers:
+        return "", 204
+    return "", 404
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
